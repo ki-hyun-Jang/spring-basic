868b5b67b764bb16794072362bcdb45615d8ffd8
ki-hyun-Jang <skjwj0511@gmail.com>
2025-01-09 17:20:16 +0900

    Controller

--- src/main/java/com/beyond/basic/b1_hello/controller/HelloController.java
+++ src/main/java/com/beyond/basic/b1_hello/controller/HelloController.java
@@ -74,4 +74,21 @@ public class HelloController {
         return "helloworld2";
     }
 
+//    case7. 화면을 return해주되, 객체를 화면에 동적으로 세팅
+    @GetMapping("/model-param2")
+    public String modelParam2(@ModelAttribute Hello hello,
+                             Model model){
+        model.addAttribute("modelHello",hello);
+        return "helloworld3";
+    }
+//    case8. pathvariable 방식을 통해 사용자로부터 값을 받아 화면 return
+//    형식: /hello/model/path/1
+//    예시: /author/detail/1
+//    pathvariable방식은 url을 통해 자우너구조를 명확하게 표현할 때 사용.(좀 더 restful한 방식)
+    @GetMapping("/model-path/{inputName}")
+    public String modelPath(@PathVariable String inputName,
+                            Model model){
+        model.addAttribute("modelName",inputName);
+        return "helloworld2";
+    }
 }
